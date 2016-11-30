1149ab059283a35e2868b12c723dde4379dc12cb
Andrew Brown <drusepth@gmail.com>
2016-11-30 04:18:35 +0100

    quor service making acceptable requests

---
+++ server/services/quora_service.rb
@@ -0,0 +1,27 @@
+class QuoraService
+
+  def self.headers
+    {
+      'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Encoding' => 'gzip, deflate, sdch, br',
+      'Accept-Language' => 'en-US,en;q=0.8',
+      'Cache-Control' => 'no-cache',
+      'Connection' => 'keep-alive',
+      'Host' => 'www.quora.com',
+      #'Origin' => 'https://www.quora.com',
+      #'Referer' => 'https://www.quora.com',
+      'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36',
+      'Upgrade-Insecure-Requests' => '1'
+    }
+  end
+
+  def self.questions question
+    HTTParty.get(search_path(question), headers: self.headers)
+  end
+
+
+  def self.search_path query
+    #todo url sanitize
+    "https://www.quora.com/search?type=question&q=#{query}"
+  end
+end
