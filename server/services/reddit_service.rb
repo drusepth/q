862b517005699cfcb7e141a37121f8d4b6c41560
Andrew Brown <drusepth@gmail.com>
2016-12-07 21:31:04 +0100

    helper function to fetch questions from reddit

--- server/services/reddit_service.rb
+++ server/services/reddit_service.rb
@@ -6,4 +6,21 @@ class RedditService
   def self.question_source_subreddits
     %w(askscience nostupidquestions)
   end
+
+  def self.current_questions_in subreddit
+    questions = []
+
+    RedditService.client.links(subreddit).each do |link|
+      link_title = link.title
+
+      if QuestionSelectionService.is_a_question? link_title
+        puts "Found question: #{link_title}"
+        questions << link_title
+      else
+        puts "Discarded question: #{link_title}"
+      end
+    end
+
+    questions
+  end
 end
